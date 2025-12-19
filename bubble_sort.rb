ea851ecfde35e68fa6e414e0a6060597d745379b
JD Alegre Casao <117078422+dyandel-dev@users.noreply.github.com>
2025-12-19 16:27:25 +0800

    Add bubble_sort.rb

---
+++ bubble_sort.rb
@@ -0,0 +1,19 @@
+def bubble_sort(array)
+  length = array.length
+
+  loop do
+    swapped = false
+
+    (length - 1).times do |index|
+      if array[index] > array[index + 1]
+        array[index], array[index + 1] = array[index + 1], array[index]
+        swapped = true
+      end
+    end
+
+    break unless swapped
+  end
+  array
+end
+
+p bubble_sort([4,3,78,2,0,2])
